983dfc4022e224e04564aa6b757a305ce2de2f4b
md-amir-gauhar <gauharamir15@gmail.com>
2020-11-30 01:48:43 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const btn = document.querySelector('.btn');
 let input = document.querySelector('.txt-input');
+const output = document.querySelector('.output');
 
 btn.addEventListener('click', (e) => {
   console.log(input.value);
+
+  fetch(
+    `https://api.funtranslations.com/translate/minion.json?text=${input.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      output.innerHTML = data.contents.translated;
+    })
+    .catch((err) => {
+      console.log(err);
+      alert(`Error: ${err}`);
+    });
 });
